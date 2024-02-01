491f0a7388002bc8ea78c4e49826b1cf3b4140fc
Fech001 <126506490+Fech001@users.noreply.github.com>
2024-02-01 20:06:20 +0000

    Add files via upload

---
+++ StackNavigation.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./TabNavigator";
+import PostScreen from "../screens/Postscreen";
+
+const Stack = createStackNavigator();
+
